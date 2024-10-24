6f4454ac733cd1bf7db4986c2f08e28a86203a29
June <khazoda@gmail.com>
2024-10-24 01:28:24 +0100

    item group reg

--- src/main/java/com/khazoda/kreebles/registry/ItemGroupRegistry.java
+++ src/main/java/com/khazoda/kreebles/registry/ItemGroupRegistry.java
@@ -8,12 +8,13 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import static com.khazoda.kreebles.registry.MainRegistry.*;
 
 public class ItemGroupRegistry {
-  public static final DeferredHolder<CreativeModeTab, CreativeModeTab> KREEBLES_TAB = ITEM_GROUPS.register("example_tab", () -> CreativeModeTab.builder()
+  public static final DeferredHolder<CreativeModeTab, CreativeModeTab> KREEBLES_TAB = ITEM_GROUPS.register("kreebles_tab", () -> CreativeModeTab.builder()
       .title(Component.translatable("itemGroup.kreebles"))
       .withTabsBefore(CreativeModeTabs.COMBAT)
-      .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+      .icon(() -> KREEBLE_STONE_ITEM.get().getDefaultInstance())
       .displayItems((parameters, output) -> {
-        output.accept(EXAMPLE_ITEM.get());
+        output.accept(KREEBLE_STONE_ITEM.get());
+        output.accept(KREEBLE_DEEPSLATE_ITEM.get());
       }).build());
 
   public static void init() {

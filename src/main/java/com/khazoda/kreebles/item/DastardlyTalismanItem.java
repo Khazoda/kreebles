d28c924f3b677b04f4d2cbfee88baedfc5c9097d
June <khazoda@gmail.com>
2024-10-25 17:28:02 +0100

    talisman tooltip fix

--- src/main/java/com/khazoda/kreebles/item/DastardlyTalismanItem.java
+++ src/main/java/com/khazoda/kreebles/item/DastardlyTalismanItem.java
@@ -8,13 +8,17 @@ import net.minecraft.world.item.TooltipFlag;
 import java.util.List;
 
 public class DastardlyTalismanItem extends Item {
-  public DastardlyTalismanItem(Properties pProperties) {
-    super(pProperties);
+  public static final  Item.Properties PROPERTIES = new Item.Properties().stacksTo(1);
+
+  public DastardlyTalismanItem(Properties p) {
+    super(PROPERTIES);
   }
 
   @Override
   public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
     super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     pTooltipComponents.add(Component.translatable("tooltip.kreebles.dastardly_talisman_1").withColor(10197915));
+    pTooltipComponents.add(Component.translatable("tooltip.kreebles.dastardly_talisman_2").withColor(10197915));
+
   }
 }
